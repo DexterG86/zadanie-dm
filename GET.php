20cbf5e26d60a2b9408fefc8b7ef26c001eac6c3
DexterG86 <90174643+DexterG86@users.noreply.github.com>
2022-03-23 10:18:53 +0100

    Add files via upload

---
+++ GET.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+GET 
+<form method="GET">
+<input type = "text" name="Li1">
+
+<input type = "text" name="Li2">
+</form>
+
+<?php
+$a=$_GET['Li1'];
+$b=$_GET['Li2'];
+
+echo $a;
+echo $b;
+?>
+
+</body>
+</html>
